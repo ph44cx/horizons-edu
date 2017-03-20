712c1cae4b289f4042eb8ff93ab70d3fbaa9b771
ph44cx <ph44cx30@gmail.com>
2017-03-20 09:29:51 +0000

    little nav test

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-    skip_before_action :authenticate_user!, :only => [ :index, :indexar ] 
+    skip_before_action :authenticate_user!, :only => [ :index, :blogs ] 
 def index
     
 end
