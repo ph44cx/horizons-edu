3aa0be78415cf6444662d7661a4ec07daee67068
ph44cx <ph44cx30@gmail.com>
2017-03-22 14:29:03 +0000

    footer ar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,9 @@ class ApplicationController < ActionController::Base
     def set_locale
       I18n.locale = params[:locale] if params[:locale].present?
     end
-   def default_url_options(options={})
-   { :locale => I18n.locale }
-   end
+    
+    def default_url_options(options={})
+    { :locale => I18n.locale }
+    end
+
 end
